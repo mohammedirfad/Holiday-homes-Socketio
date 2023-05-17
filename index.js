f1bd54206482db426fbc060ff5e80f942e729732
Mohammed irfad <muhammedirfad43@gmail.com>
2023-05-17 19:43:37 +0530

    property added2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(8800, {
     cors: {
-      origin: "https://main.dumi2cpm1nms5.amplifyapp.com/",
+      origin: "https://main.dumi2cpm1nms5.amplifyapp.com",
     },
   });
   
