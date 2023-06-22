5e529ddf684524faa2f16f4db4b294c6cf131c2f
Mohammed irfad <muhammedirfad43@gmail.com>
2023-06-22 21:41:08 +0530

    socket  completed

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ const io = require("socket.io")(8800, {
   let activeUsers = [];
   
   io.on("connection", (socket) => {
-    // add new User
+   
     socket.on("new-user-add", (newUserId) => {
-      // if user is not added previously
+     
       if (!activeUsers.some((user) => user.userId === newUserId)) {
         activeUsers.push({ userId: newUserId, socketId: socket.id });
         console.log("New User Connected", activeUsers);
       }
       console.log("New User Connected123", activeUsers);
-      // send all active users to new user
+   
       io.emit("get-users", activeUsers);
     });
   
     socket.on("disconnect", () => {
-      // remove user from active users
+     
       activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
       console.log("User Disconnected", activeUsers);
-      // send all active users to all users
+    
       io.emit("get-users", activeUsers);
     });
   
-    // send message to a specific user
+   
     socket.on("send-message", (data) => {
       const { receiverId } = data;
       const user = activeUsers.find((user) => user.userId === receiverId);
